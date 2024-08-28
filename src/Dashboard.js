5ae86d48175b3adc0a2a11dab0797b82b2c13ace
DIPESHSHETTY <dipeshshetty247@gmail.com>
2024-08-28 15:35:08 +0530

    FIRST COMMIT

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -23,6 +23,7 @@ const Dashboard = () => {
     } catch (error) {
       console.error('Error fetching student data:', error);
       // Handle the error gracefully, e.g., display an error message or redirect
+      //hi
     }
   }, []);
 
